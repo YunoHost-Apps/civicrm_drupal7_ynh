a0350b1bd042d6eff316333899ff705fd8380c11
yalh76 <yalh@yahoo.com>
2023-08-03 00:59:16 +0200

    Upgrade to 5.63.2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,9 @@
 # PHP APP SPECIFIC
 #=================================================
 
-php_dependencies="php$YNH_DEFAULT_PHP_VERSION-fpm php$YNH_DEFAULT_PHP_VERSION-cli php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-mysql php$YNH_DEFAULT_PHP_VERSION-xml php$YNH_DEFAULT_PHP_VERSION-ldap php$YNH_DEFAULT_PHP_VERSION-mbstring php$YNH_DEFAULT_PHP_VERSION-curl php$YNH_DEFAULT_PHP_VERSION-soap"
+YNH_PHP_VERSION="8.1"
+
+php_dependencies="php$YNH_PHP_VERSION-fpm php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mysql php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-ldap php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-soap php$YNH_PHP_VERSION-intl"
 
 # dependencies used by the app (must be on a single line)
 pkg_dependencies="curl libzip-dev $php_dependencies"
